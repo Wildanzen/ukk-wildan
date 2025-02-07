f8d4adafa53c83cfc057f5da71a1b439b9eab55d
Jujunzy <arjunaprasetya63@gmail.com>
2025-02-07 07:38:00 +0700

    jijiji

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,112 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="container-fluid">
+    <div class="row">
+        <!-- Sidebar/Navbar -->
+        <nav class="col-md-2 d-none d-md-block bg-primary text-white sidebar">
+            <div class="sidebar-sticky">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link text-white active" href="#">
+                            <i class="fas fa-home"></i> Dashboard
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('kategori.index') }}">
+                            <i class="fas fa-list"></i> Kategori
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('barang.index') }}">
+                            <i class="fas fa-box"></i> Barang
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('supplier.index') }}">
+                            <i class="fas fa-truck"></i> Supplier
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('pembelian.index') }}">
+                            <i class="fas fa-shopping-cart"></i> Pembelian
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-white" href="{{ route('penjualan.index') }}">
+                            <i class="fas fa-cash-register"></i> Penjualan
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <!-- Main Content -->
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+            <div
+                class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">Dashboard</h1>
+            </div>
+
+            <!-- Cards Section -->
+            <div class="row">
+                <!-- Kategori Card -->
+                <div class="col-md-4 mb-4">
+                    <a href="{{ route('kategori.index') }}" class="text-decoration-none">
+                        <div class="card shadow border-0">
+                            <div class="card-body text-center">
+                                <i class="fas fa-list fa-2x text-primary mb-3"></i>
+                                <h5 class="card-title">Kelola Kategori</h5>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </a>
+                </div>
+                <!-- Barang Card -->
+                <div class="col-md-4 mb-4">
+                    <a href="{{ route('barang.index') }}" class="text-decoration-none">
+                        <div class="card shadow border-0">
+                            <div class="card-body text-center">
+                                <i class="fas fa-box fa-2x text-success mb-3"></i>
+                                <h5 class="card-title">Kelola Barang</h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <!-- Supplier Card -->
+                <div class="col-md-4 mb-4">
+                    <a href="{{ route('supplier.index') }}" class="text-decoration-none">
+                        <div class="card shadow border-0">
+                            <div class="card-body text-center">
+                                <i class="fas fa-truck fa-2x text-warning mb-3"></i>
+                                <h5 class="card-title">Kelola Supplier</h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <!-- Pembelian Card -->
+                <div class="col-md-4 mb-4">
+                    <a href="{{ route('pembelian.index') }}" class="text-decoration-none">
+                        <div class="card shadow border-0">
+                            <div class="card-body text-center">
+                                <i class="fas fa-shopping-cart fa-2x text-danger mb-3"></i>
+                                <h5 class="card-title">Kelola Pembelian</h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <!-- Penjualan Card -->
+                <div class="col-md-4 mb-4">
+                    <a href="{{ route('penjualan.index') }}" class="text-decoration-none">
+                        <div class="card shadow border-0">
+                            <div class="card-body text-center">
+                                <i class="fas fa-cash-register fa-2x text-info mb-3"></i>
+                                <h5 class="card-title">Kelola Penjualan</h5>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
-        </div>
+        </main>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,11 +12,41 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <style>
+        /* Custom Styles for Sidebar */
+        .sidebar {
+            height: 100vh;
+            position: fixed;
+            top: 0;
+            left: 0;
+            background-color: #0d6efd;
+            color: white;
+            padding-top: 20px;
+        }
+
+        .sidebar .nav-link {
+            color: white;
+            font-weight: bold;
+            margin-bottom: 10px;
+        }
+
+        .sidebar .nav-link:hover {
+            color: #ffdd57;
+        }
+
+        .main-content {
+            margin-left: 220px;
+        }
+    </style>
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
@@ -23,19 +54,18 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
+                    <ul class="navbar-nav me-auto"></ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -50,14 +80,14 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -72,9 +102,53 @@
             </div>
         </nav>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <div class="container-fluid">
+            <div class="row">
+                <!-- Sidebar -->
+                <nav class="col-md-2 d-none d-md-block sidebar">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="{{ url('/home') }}">
+                                <i class="fas fa-home"></i> Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('kategori.index') }}">
+                                <i class="fas fa-list"></i> Kategori
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('barang.index') }}">
+                                <i class="fas fa-box"></i> Barang
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('supplier.index') }}">
+                                <i class="fas fa-truck"></i> Supplier
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('pembelian.index') }}">
+                                <i class="fas fa-shopping-cart"></i> Pembelian
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('penjualan.index') }}">
+                                <i class="fas fa-cash-register"></i> Penjualan
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <!-- Main Content -->
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 main-content">
+                    <div class="py-4">
+                        @yield('content')
+                    </div>
+                </main>
+            </div>
+        </div>
     </div>
 </body>
+
 </html>
